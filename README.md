d7f29d04c4d30fe1485eb50c081dd6cc2e2b5a90
Aislinn Hayes <aislinn.m.b.hayes@gmail.com>
2024-02-16 09:34:01 +0000

    try a few vite options to see if it applies coverage

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ To run locally:
 `npm install`
 
 `npm run test:report` to see that built "is-even" package has no coverage, even though "is-odd" uses it and has a test that should cover it.
+
--- vitest.shared.ts
+++ vitest.shared.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -6,9 +7,18 @@ export default defineConfig({
       reporter: ['lcov', 'html'],
       reportOnFailure: true,
       exclude: ['**/*.stories*', '**vitest**'],
+      allowExternal: true,
     },
     testTimeout: 60000,
     reporters: ['verbose'],
     passWithNoTests: true,
+    deps: {
+      moduleDirectories: ['node_modules', path.resolve('../../packages')],
+    },
+    server: {
+      deps: {
+        external: ['is-even'],
+      }
+    }
   },
 });
